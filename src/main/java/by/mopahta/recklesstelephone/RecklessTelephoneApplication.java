5653fa5d05a33cfa339ddcc011b6f448093115d0
Mopahta <novoseletskiy03@mail.ru>
2022-11-08 22:06:31 +0300

    init commit

---
+++ src/main/java/by/mopahta/recklesstelephone/RecklessTelephoneApplication.java
@@ -0,0 +1,13 @@
+package by.mopahta.recklesstelephone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecklessTelephoneApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(RecklessTelephoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/mopahta/recklesstelephone/RecklessTelephoneApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mopahta.recklesstelephone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecklessTelephoneApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
